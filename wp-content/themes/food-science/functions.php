1670e6a9d47431bcb9eaad5b132c0f4258d980d5
a-san-un <181435805+a-san-un@users.noreply.github.com>
2026-02-12 17:43:31 +0900

    style(theme): WordPress コーディング規約に準拠（K&R スタイル）
    - .vscode/settings.json に intelephense.format.braces 設定を追加
    - functions.php の波括弧を K&R スタイルに変更
    - 関数宣言の波括弧を同じ行に配置
    
    変更ファイル:
    - .vscode/settings.json: K&R スタイルのフォーマット設定
    - wp-content/themes/food-science/functions.php: 波括弧位置の修正
    
    Ref: WordPress Coding Standards

--- wp-content/themes/food-science/functions.php
+++ wp-content/themes/food-science/functions.php
@@ -10,8 +10,7 @@ add_theme_support('title-tag');
  * titleタグの区切り文字を変更する
  */
 add_filter('document_title_separator', 'my_document_title_separator');
-function my_document_title_separator($sep)
-{
+function my_document_title_separator($sep) {
     // $sep = '|';
     // return $sep;
     return '|';
